e2ffc96e76ecfb42e085f369e251c3571dc1c510
sardewi461 <sardewi461@gmail.com>
2023-10-14 10:41:12 +0700

    dashboard karyawan

--- application/views/karyawan/index.php
+++ application/views/karyawan/index.php
@@ -12,14 +12,17 @@
     <?php $this->load->view('components/sidebar_karyawan'); ?>
     <div class="main m-4">
         <div class="container w-75">
-            <div class="row justify-content-end">
+            <div class="row justify-content-end mb-3">
                 <div class="col-md-4 mb-3">
                     <div class="card">
                         <div class="card-header">
                             <h5 class="text-center">Masuk Kerja</h5>
                         </div>
                         <div class="card-body">
-                            <h4><?php echo $total_absen; ?> Hari</h4>
+                            <h4 class="d-flex justify-content-between">
+                                <i class="fa-solid fa-calendar-day fa-2xl mt-2"></i>
+                                <?php echo $total_absen; ?> Hari
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -29,7 +32,10 @@
                             <h5 class="text-center">Izin Kerja</h5>
                         </div>
                         <div class="card-body">
-                            <h4><?php echo $total_izin; ?> Hari</h4>
+                            <h4 class="d-flex justify-content-between">
+                                <i class="fa-solid fa-calendar-day fa-2xl mt-2"></i>
+                                <?php echo $total_izin; ?> Hari
+                            </h4>
                         </div>
                     </div>
                 </div>
@@ -39,13 +45,54 @@
                             <h5 class="text-center">Total</h5>
                         </div>
                         <div class="card-body">
-                            <h4><?php echo $absensi; ?> Hari</h4>
+                            <h4 class="d-flex justify-content-between">
+                                <i class="fa-solid fa-calendar-day fa-2xl mt-2"></i>
+                                <?php echo $absensi_count; ?> Hari
+                            </h4>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5>Histori Absen</h5>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Kegiatan</th>
+                                    <th scope="col">Tanggal</th>
+                                    <th scope="col">Jam Masuk</th>
+                                    <th scope="col">Jam Pulang</th>
+                                    <th scope="col">Keterangan</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $no = 0; foreach ($absensi as $row) : $no++; if($no > 10) break;?>
+                                <tr>
+                                    <td><?php echo $no ?></td>
+                                    <td><?php echo $row->kegiatan ?></td>
+                                    <td><?php echo $row->date ?></td>
+                                    <td><?php echo $row->jam_masuk ?></td>
+                                    <td><?php echo $row->jam_pulang ?></td>
+                                    <td><?php echo $row->keterangan_izin ?></td>
+                                </tr>
+                                <?php endforeach ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-end">
+                        <a href="<?php echo base_url('karyawan/absen') ?>" class="btn btn-success"><i
+                                class="fas fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
 </body>
 
 </html>
